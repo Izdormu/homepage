fbdc60fe4a8b9ff3505addd0a7c54bd66a970ac2
Vlad <billymosh@gmail.com>
2023-01-14 23:33:05 +0100

    added script and function for toggle language

---
+++ script.js
@@ -0,0 +1,19 @@
+
+const uaLang = document.querySelector('.ua_lang');
+const enLang = document.querySelector('.en_lang');
+const toggleLangBtn = document.getElementById("toggleLangBtn");
+const ukrainian = "Change to ukrainian"
+const english = "Змінити на англійську"
+
+toggleLangBtn.addEventListener('click', toggleLangMode);
+
+function toggleLangMode() {
+  enLang.hidden = !enLang.hidden;
+  uaLang.hidden = !uaLang.hidden;
+  if(toggleLangBtn.innerHTML === english) {
+    toggleLangBtn.innerHTML = ukrainian;
+  } else {
+    toggleLangBtn.innerHTML = english;
+  }
+}
+  
